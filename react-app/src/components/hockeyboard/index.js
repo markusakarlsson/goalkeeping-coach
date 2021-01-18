9b2cc854b3bf6ad103e361d233cf6f0f6d9df5df
Markus Karlsson <markuskarlsson__@hotmail.com>
2021-01-18 15:19:28 +0100

    name changes

--- react-app/src/components/hockeyboard/index.js
+++ react-app/src/components/hockeyboard/index.js
@@ -2,11 +2,11 @@ import React, { createRef, useRef } from "react";
 import { render } from "react-dom";
 import { Stage, Layer, Image } from "react-konva";
 import useImage from "use-image";
-import goal1 from "../../assets/goal-1.png";
-import goal2 from "../../assets/goal-2.png";
-import save3 from "../../assets/save-3.png";
-import save4 from "../../assets/save-4.png";
-import save5 from "../../assets/save-5.png";
+import goal1 from "../../assets/Goal-1.png";
+import goal2 from "../../assets/Goal-2.png";
+import save3 from "../../assets/Save-3.png";
+import save4 from "../../assets/Save-4.png";
+import save5 from "../../assets/Save-5.png";
 import UndoIcon from '@material-ui/icons/Undo';
 import "./style.scss";
 

77a00984f3f6b242ae4ce0737ac71e97f0a41b6a
Markus Karlsson <markuskarlsson__@hotmail.com>
2021-01-22 19:57:46 +0100

    Fixed bug. Added sv % and titles.

--- react-app/src/components/hockeyboard/index.js
+++ react-app/src/components/hockeyboard/index.js
@@ -47,6 +47,7 @@ const HockeyBoardSketch = () => {
   const nrOfGoals = nrOfGoal1.length + nrOfGoal2.length;
   const nrOfSaves = nrOfShots - nrOfGoals;
   const finalGrade = totalGrade / nrOfShots;
+  const savePercentage = nrOfSaves / nrOfShots;
 
   return (
     <>
@@ -60,25 +61,26 @@ const HockeyBoardSketch = () => {
         <div className="date"></div>
         <p>18/1 2021</p>
       </div>
-      <div
-        onDrop={(e) => {
-          e.preventDefault();
-          console.log(dragUrl.current.id);
-          stageRef.current.setPointersPositions(e);
-          setImages(
-            images.concat([
-              {
-                ...stageRef.current.getPointerPosition(),
-                id: imageId.current,
-                src: dragUrl.current,
-              },
-            ])
-          );
-          console.log("innan undo", images);
-        }}
-        onDragOver={(e) => e.preventDefault()}
-      >
-        <div className="hockeyBoard">
+      <div className="hockeyboard-container">
+        <div
+          onDrop={(e) => {
+            e.preventDefault();
+            console.log(dragUrl.current.id);
+            stageRef.current.setPointersPositions(e);
+            setImages(
+              images.concat([
+                {
+                  ...stageRef.current.getPointerPosition(),
+                  id: imageId.current,
+                  src: dragUrl.current,
+                },
+              ])
+            );
+            console.log("innan undo", images);
+          }}
+          onDragOver={(e) => e.preventDefault()}
+        >
+          {/* <div > */}
           <Stage width={571} height={610} ref={stageRef}>
             <Layer
               onDblClick={(e) => {
@@ -111,103 +113,125 @@ const HockeyBoardSketch = () => {
               })}
             </Layer>
           </Stage>
-          <div className="tool-container">
-            <p className="tool-instructions">
-              <TouchAppIcon /> Drag and drop shots onto the board • Double-click
-              on shot to remove
-            </p>
-            <div className="draw-container">
-              <div className="shot-container">
-                <p>Goal 1</p>
-                <img
-                  alt="Goal-1"
-                  id="goal-1"
-                  src={goal1}
-                  draggable="true"
-                  onDragStart={(e) => {
-                    dragUrl.current = e.target.src;
-                    imageId.current = e.target.id;
-                  }}
-                />
-              </div>
-              <div className="shot-container">
-                <p>Goal 2</p>
-                <img
-                  id="goal-2"
-                  alt="Goal-2"
-                  src={goal2}
-                  draggable="true"
-                  onDragStart={(e) => {
-                    dragUrl.current = e.target.src;
-                    imageId.current = e.target.id;
-                  }}
-                />
-              </div>
-              <div className="shot-container">
-                <p>Save 3</p>
-                <img
-                  id="save-3"
-                  alt="Save-3"
-                  src={save3}
-                  draggable="true"
-                  onDragStart={(e) => {
-                    dragUrl.current = e.target.src;
-                    imageId.current = e.target.id;
-                  }}
-                />
-              </div>
-              <div className="shot-container">
-                <p>Save 4</p>
-                <img
-                  id="save-4"
-                  alt="Save-4"
-                  src={save4}
-                  draggable="true"
-                  onDragStart={(e) => {
-                    dragUrl.current = e.target.src;
-                    imageId.current = e.target.id;
-                  }}
-                />
-              </div>
-              <div className="shot-container">
-                <p>Save 5</p>
-                <img
-                  id="save-5"
-                  alt="Save-5"
-                  src={save5}
-                  draggable="true"
-                  onDragStart={(e) => {
-                    dragUrl.current = e.target.src;
-                    imageId.current = e.target.id;
-                  }}
-                />
-              </div>
-              <div className="undo-container">
-                <button
-                  onClick={(e) => {
-                    e.preventDefault();
-                    images.pop();
-                    const updatedCanvas = images.slice();
-                    setUpdatedCanvas(updatedCanvas);
-                    console.log("images efter undo", images);
-                    console.log("canvas efter undo", updatedCanvas);
-                  }}
-                >
-                  Undo
-                  <UndoIcon></UndoIcon>
-                </button>
-              </div>
+          {/* </div> */}
+        </div>
+        <div className="tool-container">
+          <p className="tool-instructions">
+            <TouchAppIcon /> Drag and drop shots onto the board • Double-click
+            on shot to remove
+          </p>
+          <div className="draw-container">
+            <div
+              title="1 = A goal that the goalie should prevent."
+              className="shot-container"
+            >
+              <p>Goal 1</p>
+              <img
+                alt="Goal-1"
+                id="goal-1"
+                src={goal1}
+                draggable="true"
+                onDragStart={(e) => {
+                  dragUrl.current = e.target.src;
+                  imageId.current = e.target.id;
+                }}
+              />
+            </div>
+            <div
+              title="2 = A goal that is acceptable to let in."
+              className="shot-container"
+            >
+              <p>Goal 2</p>
+              <img
+                id="goal-2"
+                alt="Goal-2"
+                src={goal2}
+                draggable="true"
+                onDragStart={(e) => {
+                  dragUrl.current = e.target.src;
+                  imageId.current = e.target.id;
+                }}
+              />
+            </div>
+            <div
+              title="3 = A save without control on balance or rebound."
+              className="shot-container"
+            >
+              <p>Save 1</p>
+              <img
+                id="save-3"
+                alt="Save-3"
+                src={save3}
+                draggable="true"
+                onDragStart={(e) => {
+                  dragUrl.current = e.target.src;
+                  imageId.current = e.target.id;
+                }}
+              />
+            </div>
+            <div
+              title="4 = A good save with control on balance and rebound."
+              className="shot-container"
+            >
+              <p>Save 2</p>
+              <img
+                id="save-4"
+                alt="Save-4"
+                src={save4}
+                draggable="true"
+                onDragStart={(e) => {
+                  dragUrl.current = e.target.src;
+                  imageId.current = e.target.id;
+                }}
+              />
+            </div>
+            <div
+              title="5 = Every breakaway or other difficult plays, such as passes across the central line, deflections or screens etc."
+              className="shot-container"
+            >
+              <p>Save 3</p>
+              <img
+                id="save-5"
+                alt="Save-5"
+                src={save5}
+                draggable="true"
+                onDragStart={(e) => {
+                  dragUrl.current = e.target.src;
+                  imageId.current = e.target.id;
+                }}
+              />
+            </div>
+            <div className="undo-container">
+              <button
+                title="Remove the last shot you dragged onto the board."
+                onClick={(e) => {
+                  e.preventDefault();
+                  images.pop();
+                  const updatedCanvas = images.slice();
+                  setUpdatedCanvas(updatedCanvas);
+                  console.log("images efter undo", images);
+                  console.log("canvas efter undo", updatedCanvas);
+                }}
+              >
+                Undo
+                <UndoIcon></UndoIcon>
+              </button>
             </div>
           </div>
         </div>
       </div>
       <div className="shot-presentation">
-        <p>
+        <p title="Number of saves / Number of shots">
           Saves/Shots = {nrOfSaves}/{nrOfShots}
         </p>
       </div>
+      <div className="save-percentage-presentation">
+        <p title="Save Percentage - The number of saves divided by the number of shots on goal.">
+          Sv % ={nrOfSaves + nrOfShots !== 0 && savePercentage.toFixed(3)}
+        </p>
+      </div>
       <div className="grade-presentation">
-        <p>
+        <p title="Grade - The amount of interventions divided by the summary of all actions. < 3.8 = Red = NOT GOOD • 3.80 - 4.09 = Green = GOOD • > 4.10 = Yellow = EXCELLENT.">
           Grade =
           {nrOfSaves + nrOfShots !== 0 &&
             " " + (totalGrade / nrOfShots).toFixed(2) + " / "}
